15c12c696b2a291943a0f1718ac63bb6db4a4757
Alexander Karlsson <alexander.karlsson@learnet.se>
2021-10-15 11:58:06 +0200

    .

--- display.js
+++ display.js
@@ -737,10 +737,11 @@ let chars = {
 }
 
 class BITMAP {
-    constructor() {
-        this.WIDTH;
-        this.HEIGHT;
-        this.PLANE;
+    constructor(width, height) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.PLANE =  new Array(width).fill(0).map(() => new Array(height).fill(0));
+        //  new Array(y).fill(0).map(() => new Array(x).fill(0));  //Inte färdig....
     }
 
     bitmapMaker(character) {
@@ -784,16 +785,12 @@ class DISPLAY {
         return c
     }
 
-    loop(func, domain) {
+    gridMap(func) {
         for (let i = 0; i < this.WIDTH; i++) {
             for (let j = 0; i < this.HEIGHT; j++) {
-                if (domain(i)(j)) {
-                    this.PLANE[this.to1D(i, j)] = func(i)(j);
-                }
+                func(i)(j);
             }
         }
-
-        this.PLANE.map(func)
     }
 
     to2DArray() {
@@ -875,17 +872,29 @@ class DISPLAY {
     }
 
     blitToDisplay(BITMAP, width, height, bx, by, dx, dy) {
-        for (let i = x; i < this.WIDTH && i - x < BITMAP.WIDTH; i++) {
-            for (let j = y; j < this.HEIGHT && j - y < BITMAP.HEIGHT; j++) {
-                if (BITMAP.PLANE[j - y][i - x] === 1) {
-                    this.PLANE[j][i] = color;
+        for (let i = 0; i < bx && i < dx; i++) {
+                for (let j = y1; j < y2; j++) {
+                    this.putPixel(i, j, color);
                 }
             }
-        }
     }
 
-    blitToBitmap(BITMAP, width, height, bx, by, dx, dy) {
-
+    blitToBitmap(BITMAP, width, height,bx, by, dx, dy) {
+        // let temp = new Array(10).fill(0).map(element => new Array(10).fill(0));
+        let temp = new BITMAP(width*bx, height*by);
+        for (let x = dx-width; x < dx; x++) {
+            for (let y = dy-height; y < dy; y++) {
+                // this.PLANE[this.to1D(x, y)] = BITMAP.PLANE[this.to1D(x, y)];
+                
+                temp.PLANE[this.to1D(x, y)] = this.PLANE[this.to1D(x, y)]; 
+                
+                // if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+                //     BITMAP.PLANE[this.to1D(x, y)] = color;
+                //     BITMAP.PLANE
+                // }
+            }
+        }
+        console.log(temp.PLANE);
     }
 
     textOut(x, y, color, string) {
@@ -1067,6 +1076,8 @@ window.addEventListener('load', () => {
     // hello.rectangle(190, 190, 210, 210, 230);
     // hello.line(190, 190, 450, 450, 200);
     // hello.line(450, 450, 470, 430, 200);
+    hello.line(0, 9,9, 0, 255);
+    // hello.line(0, 0, 10, 10, 255);
     // hello.line(450, 450, 430, 445, 200);
     hello.line(0, 0, 10, 10, 255);
     // hello.putPixel(40,40,255);
